4df30c11c4ea737de214facdc8dbec5c58c94059
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-10-18 15:59:13 -0300

    inciando rn

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Home from './src/screens/Home';
+import Register from './src/screens/register';
+function App() {
+  return (
+    <View>
+      <Home />
+      <Register />
+    </View>
+  );
+}
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text,} from 'react-native';
+
+class Home extends Component{
+    constructor(){
+        super();
+        this.state ={
+        }
+    }
+    render(){
+        return(
+            <View>
+               <Text></Text>
+            </View>
+        )
+    }
+}
+
+export default Home;
