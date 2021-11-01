9e130bd4cb9df9e1b2e01bd54bd0be655441c142
juanhuidobro <juan.huidobro@sanandres.esc.edu.ar>
2021-11-01 17:12:43 -0300

    login

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
 
-export default class Login extends Component{
+class Login extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            email : '',
+            password: ''
+        }
     }
     render(){
         return(
-            <View>
-               <Text>Ingresar</Text>
-            </View>
+            <View style= {styles.container}>
+            <Text style={styles.titulo}>Iniciar session</Text>
+            <TextInput
+                style ={styles.input}
+                placeholder = 'email'
+                keyboardType = 'email-address'
+                onChangeText = { (text) => this.setState({email: text})} 
+            />
+            <TextInput
+                style ={styles.input}
+                placeholder = 'password'
+                keyboardType = 'default'
+                secureTextEntry = {true}
+                onChangeText = { (text) => this.setState({password: text})} 
+            />
+            <TouchableOpacity style = {styles.boton} onPress={() => this.props.login(this.state.email, this.state.password)}>
+                <Text style={styles.enviar}>Ingresar</Text>
+            </TouchableOpacity>
+        </View>
         )
-    }
+    }  
 }
+const styles = StyleSheet.create({
+})
+export default Login;
